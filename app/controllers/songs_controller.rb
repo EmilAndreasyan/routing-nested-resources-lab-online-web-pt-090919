82c2b644d612de8e72797b362b31230f863f53c2
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-02-19 04:56:29 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,9 +4,11 @@ class SongsController < ApplicationController
      @artist = Artist.find_by(id: params[:artist_id])
      if @artist.nil?
        redirect_to artists_path, alert: "artist not found"
+      else
+        @songs = @artist.songs
      end
    else
-     
+     @songs = Song.all
    end
   end
 
