93192dd4a24caf8754860ba3668f72f00b67ba87
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-02-19 04:34:27 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,7 @@
 class SongsController < ApplicationController
   def index
     if params[:artist_id]
-      @songs = Artist.find(params[:artist_id]).songs
+      @artist = Artist.find_by(id: params[:id])
       redirect_to artists_path
     else
     @songs = Song.all
